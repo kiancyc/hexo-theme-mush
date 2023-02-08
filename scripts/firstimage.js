9085217023fd2c6de9257ed02e53ab1f414de156
Martin <martin_cai@qq.com>
2023-02-08 15:23:03 +0800

    up firstimage

--- scripts/firstimage.js
+++ scripts/firstimage.js
@@ -1,9 +1,10 @@
 // get first image src from post
 hexo.extend.helper.register('firstimage', function(post, defaultImage = "/image/thumbnail.png") {
 
-    let firstImage = post.content.replace(/.*?src="([^"]*)".*/,'$1')
+    // let firstImage = post.content.replace(/.*?src="([^"]*)".*/,'$1')
+    let firstImage = post.content.match(/src\s*=\s*"(.+?)"/)[1]
 
-    if(firstImage)    
+    if(firstImage)
         return '/' + post.path + firstImage;
     else
         return defaultImage
